131c6cc00c4082466e0bd9a2802b63a1d44172bf
mattkielar <matthewkielar16@gmail.com>
2023-01-21 17:42:52 -0800

    function for kernel regression

---
+++ functions/kernel_smoother.R
@@ -0,0 +1,22 @@
+#' @param x A vector of values for the predictor variable
+#' @param y A vector of values for the response variable
+#' @param kernel The kernel to smooth the data with
+#' @param bandwidth The coverage range of the kernel
+
+kernel_smoother <- function(x, y, kernel = c("gaussian", "uniform"), bandwidth) {
+    dist <- as.matrix(dist(df$x))
+    kernel <- match.arg(kernel)
+
+    # Ensure specified kernel is valid
+    if (kernel %in% c("gaussian", "uniform") == FALSE) {
+        stop("specified kernel must be either gaussian or uniform")
+    }
+    # Fit kernel regression
+    S <- switch(
+        kernel,
+        gaussian = dnorm(dist, mean = 0, sd = bandwidth * 0.3706506),
+        uniform = dist <= (bandwidth * 0.5))
+    S <- sweep(S, 1, rowSums(S), "/")
+    mod_fit <- S %*% y
+    return(mod_fit)
+}
