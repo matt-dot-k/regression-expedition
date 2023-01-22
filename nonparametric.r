c74bd12f498d3495110689a66bb7a3d1b6a4b5ec
mattkielar <matthewkielar16@gmail.com>
2023-01-21 21:47:52 -0800

    fit smoothers, CV for bandwidth

--- nonparametric.r
+++ nonparametric.r
@@ -17,8 +17,7 @@ data <- cbind(x, y) %>% as.data.frame()
 # Plot true regression function
 wiggly_plot <- ggplot(
     data = data,
-    mapping = aes(
-        x = x)
+    aes(x = x)
 ) +
     geom_point(
         aes(y = y),
@@ -28,13 +27,62 @@ wiggly_plot <- ggplot(
     stat_function(
         fun = wiggly_function,
         color = "#000000",
-        size = 1.2
+        linewidth = 1.2
 ) +
     labs(
-        x = "x",
-        y = "y",
         title = "Plot of True Regression Function"
 )
 
 wiggly_plot
 
+source("./functions/kernel_smoother.R")
+
+# Fit kernel regression
+uniform <- kernel_smoother(x = data$x, y = data$y, "uniform", 3)
+gaussian <- kernel_smoother(x = data$x, y = data$y, "gaussian", 3)
+
+smoothers <- cbind(data, uniform, gaussian)
+
+colors <- c("Truth" = "#000000", "Uniform" = "#0F5499", "Gaussian" = "#990F3D")
+
+# Plot model fits
+smoother_plot <- ggplot(
+    data = smoothers,
+    aes(x = x)
+) +
+    geom_point(
+        aes(y = y),
+        color = "gray50",
+        size = 1.2
+) +
+    geom_line(
+        aes(y = uniform,
+            color = "Uniform"),
+        linewidth = 1.2
+) +
+    geom_line(
+        aes(y = gaussian,
+            color = "Gaussian"),
+        linewidth = 1.2
+) +
+    stat_function(
+        aes(color = "Truth"),
+        fun = wiggly_function,
+        linewidth = 1.2
+) +
+    labs(
+        title = "Fitted Kernel Regression Estimates",
+        color = "Function"
+) +
+    scale_color_manual(
+        values = colors
+)
+
+smoother_plot
+
+# Cross validation to choose optimal bandwidth
+source("./functions/mean_sq_err.R")
+source("./functions/cross_val.R")
+
+cross_val(x = data$x, y = data$y, "uniform", n_iter = 20)
+cross_val(x = data$x, y = data$y, "gaussian", n_iter = 20)
